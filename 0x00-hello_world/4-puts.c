aa6e67f43f02577bc2ada1e20364016d639d0e3c
Ita Enang <enangita@gmail.com>
2023-09-07 15:06:46 -0700

    added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
