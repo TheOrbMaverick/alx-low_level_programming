b9141230930944a8ff3a44971060c4174179713b
Ita Enang <enangita@gmail.com>
2023-09-08 05:32:59 -0700

    things to do in c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers in base 10.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar(digit + '0'); // Convert the digit to its character representation
+    }
+
+    putchar('\n');
+
+    return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers in base 10 without using char.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num = 0;
+
+    while (num < 10)
+    {
+        putchar(num + '0');
+        num++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
