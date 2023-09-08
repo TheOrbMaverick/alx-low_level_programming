32beb7082a7866f81f8b3824d592a75e40690b93
Ita Enang <enangita@gmail.com>
2023-09-08 07:09:28 -0700

    betty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,10 +10,13 @@
 int main(void)
 {
 	int digit;
-	for (digit = 0; digit <= 9; digit++){
+
+	for (digit = 0; digit <= 9; digit++)
+	{
 		putchar(digit + '0'); /* Convert the digit to its character representation */
 	}
 
 	putchar('\n');
+
 	return (0);
 }
