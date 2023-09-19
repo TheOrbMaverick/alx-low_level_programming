19cb3a35fe7abb095048a52e6f6a2eaf3738a561
Ita Enang <enangita@gmail.com>
2023-09-19 11:49:40 -0700

    check

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: Pointer to a null-terminated string.
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i += 2; // Move to the next character (every other character)
+	}
+
+	write(1, "\n", 1);
+}
