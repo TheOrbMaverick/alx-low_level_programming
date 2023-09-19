d5108d17d3cff7cc7f8950d26a6c5dbc23e778ae
Ita Enang <enangita@gmail.com>
2023-09-19 09:42:43 -0700

    betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,9 +8,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	int temp; /* Declare a temporary variable to hold the value */
+    int temp; /* Declare a temporary variable to hold the value */
 
-	temp = *a; /* Store the value pointed to by a in temp */
-	*a = *b;   /* Update the value pointed to by a with the value pointed to by b */
-	*b = temp; /* Update the value pointed to by b with the value in temp */
+    temp = *a; /* Store the value pointed to by a in temp */
+    *a = *b;   /* Update the value pointed to by a with the value pointed to by b */
+    *b = temp; /* Update the value pointed to by b with the value in temp */
 }
