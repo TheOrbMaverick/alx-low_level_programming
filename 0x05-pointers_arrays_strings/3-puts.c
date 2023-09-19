4235843de8ba050174b196f37b111e7773142746
Ita Enang <enangita@gmail.com>
2023-09-19 10:15:28 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdout.
+ * @str: Pointer to a null-terminated string.
+ */
+void _puts(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+	write(1, "\n", 1);
+}
