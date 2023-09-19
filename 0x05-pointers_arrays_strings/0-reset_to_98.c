436503f383b1322a22c71500e651ae93488cb9a2
Ita Enang <enangita@gmail.com>
2023-09-19 09:28:51 -0700

    check

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
  * reset_to_98 - Updates the value pointed to by a pointer to 98.
  * @n: Pointer to an integer.
  */
-void reset_to_98(int *n) 
+void reset_to_98(int *n)
 {
-    *n = 98; /* Dereference the pointer and update the value to 98 */
+	*n = 98; /* Dereference the pointer and update the value to 98 */
 }
