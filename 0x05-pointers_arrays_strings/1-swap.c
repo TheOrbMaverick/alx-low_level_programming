9b1f513aa75a05d7c0913502eba397d9aeb6dad0
Ita Enang <enangita@gmail.com>
2023-09-19 09:55:57 -0700

    betty

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,12 @@
 /**
- * swap_int - Swaps the values of two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- *
- * Description: This function takes two integer pointers and swaps
- * the values of the integers they point to.
- */
+ * swap_int - Swaps the values of two integers
+ * Description: This function takes two integer pointers and swaps them 
+*/
 void swap_int(int *a, int *b)
 {
-    int temp; /* Declare a temporary variable to hold the value */
+	int temp; /* Declare a temporary variable to hold the value */
 
-    temp = *a; /* Store the value pointed to by a in temp */
-    *a = *b;   /* Update the value pointed to by a with the value pointed to by b */
-    *b = temp; /* Update the value pointed to by b with the value in temp */
+	temp = *a; /* Store the value pointed to by a in temp */
+	*a = *b; /* Update the value pointed to by a with the value pointed to by b */
+	*b = temp; /* Update the value pointed to by b with the value in temp */
 }
