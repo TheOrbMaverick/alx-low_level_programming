1af0cf4611b4a1de05d66a8dfb7e370f2fb27b0d
Ita Enang <enangita@gmail.com>
2023-09-21 03:45:40 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * _strncpy - Copies a string.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: Maximum number of bytes to copy from src.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0'; /* Fill remaining bytes with null characters */
+		i++;
+	}
+
+	return dest;
+}
