935b6514b984aac25bd2320fcc0fa24fe97fe69e
Ita Enang <enangita@gmail.com>
2023-09-25 13:39:09 -0700

    check

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,8 +3,7 @@
  * @s: The string to search.
  * @c: The character to locate.
  *
- * Return: A pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found.
+ * Return: Pointer to the first occurrence of c in s, or NULL if not found.
  */
 char *_strchr(char *s, char c)
 {
