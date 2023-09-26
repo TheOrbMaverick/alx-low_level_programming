99aaf91d96042386d7c6bbff6b9a5661e5d34176
Ita Enang <enangita@gmail.com>
2023-09-26 12:06:23 -0700

    check

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif /* MAIN_H */
