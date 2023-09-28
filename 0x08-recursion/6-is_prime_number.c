840e44f47c5c8f3df7c0e23f3a0ee5284c0dd9e5
Ita Enang <enangita@gmail.com>
2023-09-28 10:56:51 -0700

    finishing

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,16 +8,16 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
-		return (0); // 0 and 1 are not prime numbers
+		return (0); /* 0 and 1 are not prime numbers */
 	}
 
 	for (int i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 		{
-			return (0); // n is divisible by i, so it's not prime
+			return (0); /* n is divisible by i, so it's not prime */
 		}
 	}
 
-	return (1); // n is prime
+	return (1); /* n is prime */
 }
