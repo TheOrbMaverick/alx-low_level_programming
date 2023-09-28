27f3651a9cc9a68a91aaebbc5db95dec8308fe7d
Ita Enang <enangita@gmail.com>
2023-09-28 11:31:30 -0700

    extra credit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,10 +4,12 @@
  *
  * Return: 1 if the string is a palindrome, 0 otherwise.
  */
+int check_palindrome(char *s, int start, int end);
+
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
-	return check_palindrome(s, 0, len - 1);
+	return (check_palindrome(s, 0, len - 1));
 }
 
 /**
@@ -21,12 +23,12 @@ int is_palindrome(char *s)
 int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
-		return 1;
+		return (1);
 
 	if (s[start] != s[end])
-		return 0;
+		return (0);
 
-	return check_palindrome(s, start + 1, end - 1);
+	return (check_palindrome(s, start + 1, end - 1));
 }
 
 /**
@@ -38,6 +40,6 @@ int check_palindrome(char *s, int start, int end)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-	return 1 + _strlen_recursion(s + 1);
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
