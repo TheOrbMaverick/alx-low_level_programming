3f85eea127597ad5209c11079167d4ad7647ad39
Ita Enang <enangita@gmail.com>
2023-09-28 05:28:55 -0700

    check

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* Function prototypes */
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+
+#endif /* MAIN_H */
