c4645452f339a2c08222f22dc9c96b8ab53c1438
Ita Enang <enangita@gmail.com>
2023-09-28 10:49:11 -0700

    check

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 /**
  * _sqrt_recursion - Calculates the natural square root of a number.
  * @n: The number for which to find the square root.
+ * @guess: The current guess for the square root.
  *
  * Return: The natural square root of n, or -1 if n does not have a
  *         natural square root.
@@ -10,9 +11,9 @@ int find_sqrt(int n, int guess);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return -1; /* Error: negative number */
+		return (-1); /* Error: negative number */
 
-	return find_sqrt(n, 0);
+	return (find_sqrt(n, 0));
 }
 
 /**
@@ -25,10 +26,10 @@ int _sqrt_recursion(int n)
 int find_sqrt(int n, int guess)
 {
 	if (guess * guess == n)
-		return guess; /* Found the square root */
+		return (guess); /* Found the square root */
 
 	if (guess * guess > n)
-		return -1; /* Error: no natural square root */
+		return (-1); /* Error: no natural square root */
 
-	return find_sqrt(n, guess + 1); /* Try the next guess */
+	return (find_sqrt(n, guess + 1)); /* Try the next guess */
 }
