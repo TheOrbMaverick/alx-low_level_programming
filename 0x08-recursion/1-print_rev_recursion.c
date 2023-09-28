700b96bf82f38ac2d599311302aae8b716193bd2
Ita Enang <enangita@gmail.com>
2023-09-28 05:24:09 -0700

    check

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "_putchar.c"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to print in reverse.
+ *
+ * Description: This function uses recursion to print the characters of a string
+ *              in reverse order until it reaches the null terminator '\0'.
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+        return;
+    }
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+}
