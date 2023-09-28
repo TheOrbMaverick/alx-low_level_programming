b9aad523dce59872c59ea8f6f3406045659425f0
Ita Enang <enangita@gmail.com>
2023-09-28 10:38:20 -0700

    check

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,10 +7,10 @@
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;  // Error: negative number
+	if (n < 0)
+		return -1; /* Error: negative number */
 
-    return find_sqrt(n, 0);
+	return find_sqrt(n, 0);
 }
 
 /**
@@ -22,11 +22,11 @@ int _sqrt_recursion(int n)
  */
 int find_sqrt(int n, int guess)
 {
-    if (guess * guess == n)
-        return guess;  // Found the square root
+	if (guess * guess == n)
+		return guess; /* Found the square root */
 
-    if (guess * guess > n)
-        return -1;  // Error: no natural square root
+	if (guess * guess > n)
+		return -1; /* Error: no natural square root */
 
-    return find_sqrt(n, guess + 1);  // Try the next guess
+	return find_sqrt(n, guess + 1); /* Try the next guess */
 }
