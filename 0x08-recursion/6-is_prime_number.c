df02e854f185d75283e410e5cd3d42fc9691a38c
Ita Enang <enangita@gmail.com>
2023-09-28 11:11:08 -0700

    prime check

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,22 @@
 /**
+ * is_prime_recursive - Checks if an integer is a prime number.
+ * @n: The integer to check.
+ * @divisor: The current divisor to check.
+ *
+ * Return: 1 if n is a prime number, 0 otherwise.
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (n < 2 || n % divisor == 0)
+		return (0);
+
+	if (divisor * divisor > n)
+		return (1);
+
+	return (is_prime_recursive(n, divisor + 1));
+}
+
+/**
  * is_prime_number - Checks if an integer is a prime number.
  * @n: The integer to check.
  *
@@ -7,17 +25,10 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
-	{
-		return (0); /* 0 and 1 are not prime numbers */
-	}
+		return (0);
 
-	for (int i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0); /* n is divisible by i, so it's not prime */
-		}
-	}
+	if (n == 2)
+		return (1);
 
-	return (1); /* n is prime */
+	return (is_prime_recursive(n, 2));
 }
