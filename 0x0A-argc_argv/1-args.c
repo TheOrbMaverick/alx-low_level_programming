2b518856a5de0e90d51c83ce2cc6c54fe820b615
Ita Enang <enangita@gmail.com>
2023-10-07 09:47:10 -0700

    check

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
 	/* Check if argc is greater than or equal to 1 */
 	if (argc >= 1)
 	{
 		/* Print the number of arguments passed, excluding the program name */
 		printf("%d\n", argc - 1);
+		printf("%d\n", *argv[0]);
 		return (0);
 	}
 	else
