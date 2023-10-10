5d117367967d9d843fa3a15b386752f81604d755
Ita Enang <enangita@gmail.com>
2023-10-10 13:52:41 -0700

    check

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,8 +3,13 @@
  * name - name of the dog
  * age - how lod the dog is
  * owner - me the cute owner*/
+#ifndef DOG_H
+#define DOG_H
+
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
