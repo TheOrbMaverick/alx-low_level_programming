92e167f743ce04ce221a781bcf6dc52b0e673f9a
Ita Enang <enangita@gmail.com>
2023-10-10 13:48:42 -0700

    check

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h> /* for memory allocation */
 #include "dog.h"
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	/* Allocate memory for the name string */
+	d->name = (char *)malloc(strlen(name) + 1); /* +1 for the null terminator */
+
+	/* Check if memory allocation was successful */
+	if (d->name != NULL)
+	{
+		strcpy(d->name, name);
+		d->age = age;
+		d->owner = owner; /* Assuming ownership is not duplicated */
+	}
+	else
+	{
+		/* Handle memory allocation failure */
+		/* You might want to free any previously allocated memory here */
+	}
 }
