693eebcdedb9b16fdb22676013eb609ef9ff5a53
Ita Enang <enangita@gmail.com>
2023-10-10 14:10:28 -0700

    check

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,22 @@
-/*
- * struct - holding variables for the doggie
- * name - name of the dog
- * age - how lod the dog is
- * owner - me the cute owner*/
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+ */
+
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/* function prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
