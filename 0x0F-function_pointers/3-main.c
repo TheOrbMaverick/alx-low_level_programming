4bc015e109aa2afcc23fa88cbc2f26c501a19cba
Ita Enang <enangita@gmail.com>
2023-10-11 15:32:21 -0700

    check

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
 	int result;
 	char *operator;
 	int (*op_func)(int, int);
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	
+
 	num1 = atoi(argv[1]);
 	operator = argv[2];
 	num2 = atoi(argv[3]);
@@ -31,9 +31,9 @@ int main(int argc, char *argv[])
 	if ((strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0) && num2 == 0)
 	{
 		printf("Error\n");
-		return 100;
+		return (100);
 	}
-	
+
 	result = op_func(num1, num2);
 	printf("%d\n", result);
 
