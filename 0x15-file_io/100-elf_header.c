656e6dfdf9f62e2b3ec0016af1ed7150ad338196
Ita Enang <enangita@gmail.com>
2023-10-30 11:22:19 -0700

    check

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -44,3 +44,13 @@ void print_elf_header_info(const char *filename) {
 
     close(fd);
 }
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+        return 98;
+    }
+
+    print_elf_header_info(argv[1]);
+    return 0;
+}
