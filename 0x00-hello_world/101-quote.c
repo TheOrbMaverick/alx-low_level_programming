99a6b75986a4c05556a00e15b655a2a37c2705bd
Ita Enang <enangita@gmail.com>
2023-11-21 11:25:33 -0800

    the quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <unistd.h>
 #define PHRASE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
-/*
-* int main - prints the phrase.
-* return - it returns 1
-*/
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message to the standard error.
+ *
+ * Return: Always 1 (indicating an error)
+ */
 
 int main(void)
 {
