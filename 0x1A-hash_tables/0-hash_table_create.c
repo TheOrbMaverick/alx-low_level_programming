c191f735b3216edf508ea69dd121ee4424d6310d
root <enangita@gmail.com>
2023-12-24 09:00:47 -0800

    wetin

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stdlib.h>
 
 /**
  * hash_table_create - Creates a hash table
@@ -6,6 +7,7 @@
  *
  * Return: A pointer to the newly created hash table, or NULL if something went wrong
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *hash_table = NULL;
