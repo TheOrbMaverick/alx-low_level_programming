1dccf0a8859f575036dbf0e96cc4ed02a3101dd8
root <enangita@gmail.com>
2023-12-24 09:14:18 -0800

    che

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,34 +4,37 @@
  * hash_table_create - Creates a hash table
  * @size: Size of the array
  *
- * Return: A pointer to the newly created hash table, or NULL if something went wrong
+ * Return: A pointer to the newly created hash table or NULL.
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
-    unsigned long int i;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (hash_table == NULL) {
-        return NULL;  /* Return NULL if allocation fails */
-    }
+	/* Allocate memory for the hash table structure */
+	hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);  /* Return NULL if allocation fails */
+	}
 
-    /* Allocate memory for the array of nodes */
-    hash_table->array = (Node **)malloc(sizeof(Node *) * size);
-    if (hash_table->array == NULL) {
-        free(hash_table);
-        return NULL;  /* Return NULL if allocation fails */
-    }
+	/* Allocate memory for the array of nodes */
+	hash_table->array = (Node **)malloc(sizeof(Node *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);  /* Return NULL if allocation fails */
+	}
 
-    /* Initialize each element in the array to NULL */
-    for (i = 0; i < size; i++) {
-        hash_table->array[i] = NULL;
-    }
+	/* Initialize each element in the array to NULL */
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
 
-    /* Set the size of the hash table */
-    hash_table->size = size;
+	/* Set the size of the hash table */
+	hash_table->size = size;
 
-    return hash_table;
+	return (hash_table);
 }
