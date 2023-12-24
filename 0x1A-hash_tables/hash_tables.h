849d93f60fcd1346024ea7028d10dd231febd901
root <enangita@gmail.com>
2023-12-24 09:40:22 -0800

    meh

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,5 +32,6 @@ typedef struct hash_table {
  * Return: A pointer to the newly created hash table, or NULL if something went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
