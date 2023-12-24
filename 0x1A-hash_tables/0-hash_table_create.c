73faceb202065f0cea40211ade57096fa9148925
root <enangita@gmail.com>
2023-12-24 13:47:39 -0800

    g

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,27 +10,27 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new_table = NULL;
-    unsigned long int i;
+	hash_table_t *new_table = NULL;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array of pointers to hash nodes */
-    new_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (new_table->array == NULL)
-    {
-        free(new_table);  /* Free the hash table structure on failure */
-        return (NULL);
-    }
+	/* Allocate memory for the array of pointers to hash nodes */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);  /* Free the hash table structure on failure */
+		return (NULL);
+	}
 
-    /* Initialize each element of the array to NULL */
-    for (i = 0; i < size; i++)
-        new_table->array[i] = NULL;
+	/* Initialize each element of the array to NULL */
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 
-    new_table->size = size;
+	new_table->size = size;
 
-    return (new_table);
+	return (new_table);
 }
