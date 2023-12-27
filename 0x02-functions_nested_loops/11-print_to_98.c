cf8c056da7d9ae35ca819b030cf077b682c615b9
root <enangita@gmail.com>
2023-12-27 14:51:59 -0800

    cheche

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -6,26 +7,26 @@
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
 
-            if (n < 98)
-                printf(", ");
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
+			if (n < 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
 
-            if (n > 98)
-                printf(", ");
-        }
-    }
+			if (n > 98)
+				printf(", ");
+		}
+	}
 
-    printf("\n");
+	printf("\n");
 }
