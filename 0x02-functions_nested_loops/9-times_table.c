3cbff3a804f8056996ce8222e1d8739334205cbe
root <enangita@gmail.com>
2023-12-27 13:52:19 -0800

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,19 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
 
-            if (j == 0)
-                printf("%d", result);
-            else
-                printf(", %d", result);
-        }
-        printf("\n");
-    }
+			if (j == 0)
+				printf("%2d", result);
+			else
+				printf(", %2d", result);
+		}
+		printf("\n");
+	}
 }
