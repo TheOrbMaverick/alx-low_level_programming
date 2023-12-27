ebfe98e8fe3bd06d64e6e5855a42c216ff5f096b
root <enangita@gmail.com>
2023-12-27 10:58:52 -0800

    fabu

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int first = 1, second = 2, next, sum = 0;
+	int first = 1, second = 2, next, sum = 0;
 
-    while (second <= 4000000)
-    {
-        if (second % 2 == 0)
-        {
-            sum += second;
-        }
+	while (second <= 4000000)
+	{
+		if (second % 2 == 0)
+		{
+			sum += second;
+		}
 
-        next = first + second;
-        first = second;
-        second = next;
-    }
+		next = first + second;
+		first = second;
+		second = next;
+	}
 
-    printf("Sum of even-valued terms in Fibonacci sequence: %d\n", sum);
+	printf("%d\n", sum);
 
-    return 0;
+	return (0);
 }
