e057d3792eb454cae1643ada7cf185d48136fb52
root <enangita@gmail.com>
2023-12-27 10:06:11 -0800

    ch

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf('+');
+		printf("%d", '+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf('0');
+		printf("%d", '0');
 		return (0);
 	}
 	else
 	{
-		printf('-');
+		printf("%d", '-');
 		return (-1);
 	}
 }
