25ba4b5a0fcda9d94e83483e96c57489185c6ea7
TheOrbMaverick <enangita@gmail.com>
2024-02-22 20:36:21 +0100

    maker

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    Function to calculate the perimeter of the island described in grid
+
+    Args:
+        grid (List[List[int]]): a list of list of integers
+
+    Returns:
+        int: the perimeter of the island
+    """
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
