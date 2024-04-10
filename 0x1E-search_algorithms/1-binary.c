d6b9721b75fb6e6753f7cd465eb81f3aa50863bf
TheOrbMaverick <enangita@gmail.com>
2024-04-10 17:44:15 +0100

    re-run

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t mid;
 	size_t left = 0;
-	size_t right = (size-1);
+	size_t right = (size - 1);
 
 	if (array == NULL)
 		return (-1);
